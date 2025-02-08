8485c2351a3e2c15b00acf472dad31b019620538
CHARAN REDDY <charanreddy.puchala@gmail.com>
2025-02-08 23:12:18 +0530

    Update c++.cpp

--- c++.cpp
+++ c++.cpp
@@ -6,8 +6,8 @@ int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     
     #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
     #endif
     int tcs = 1;
     //cin >> tcs;
